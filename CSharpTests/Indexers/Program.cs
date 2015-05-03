5a5e116b770922d72428365c24636bb610ab526c
Ronald Wiedijk <ronaldwiedijk@gmail.com>
2015-05-03 14:35:30 +0200

    indexer implemented

--- CSharpTests/Indexers/Program.cs
+++ CSharpTests/Indexers/Program.cs
@@ -10,6 +10,29 @@ namespace Indexers
     {
         static void Main(string[] args)
         {
+            var s = new MySentence();
+
+            {
+                Console.WriteLine(s[3]);
+            }
+            
+            Console.Read();
         }
+
+        class MySentence
+        {
+            string[] words = "Het is vandaag Zondag 3 Mei, over 2 dagen is het bevrijdingsdag!".Split();
+
+            public string this[int index]
+            {
+                get { return words[index]; }
+                set { words[index] = value; }
+            }
+
+        }
+
+
+
+
     }
 }
