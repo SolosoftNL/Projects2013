d320ceb352842eca32a0b8d0df53cec3cdab7020
Ronald Wiedijk <ronaldwiedijk@gmail.com>
2015-05-03 13:53:03 +0200

    Indexer toegevoegd

---
+++ CSharpTests/Events2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+        }
+
+        public event NumberReachedEventHandler NumberReached;
+
+
+        public delegate void NumberReachedEventHandler(object sender, NumberReachedEventArgs e);
+
+
+
+
+    }
+}
---
+++ CSharpTests/ParametersByRefAndOut/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersByRefAndOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Getal = 34;
+
+
+            PrintByValue(Getal);
+            Console.WriteLine("Parameter by Val: " + Getal.ToString());
+
+            PrintValueByRef(out Getal);
+            Console.WriteLine("Parameter by Ref: " + Getal.ToString());
+
+            int i = 33;
+            string s1 = "Hello";
+            string s2 = "Ronald";
+            Method(out i,out s1,out s2);
+
+            Console.ReadKey();
+
+        }
+
+        private static void PrintValueByRef(out int Value)
+        {
+            Value = 8;
+            
+        }
+
+        private static void PrintByValue(int Value)
+        {
+            Value = 10;
+           
+        }
+
+        static void Method(out int i, out string s1, out string s2)
+        {
+            i = 44;
+            s1 = "I've been returned";
+            s2 = null;
+        }
+
+
+
+    }
+}
